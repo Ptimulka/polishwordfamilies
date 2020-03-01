2eefe1ebec61f661faa1f1d13ec463b1eb385325
Marta <ptimuleczka@gmail.com>
2020-03-01 15:24:13 +0100

    Add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var cors = require('cors');
 var app = express();
 
 const PORT = process.env.PORT || 3000
@@ -10,25 +11,27 @@ function searchWord(word) {
 
     for(var i=word.length; i>0; i=i-1) {
 
-      let prefix = word.substring(0,i);
-      let suffix = word.substring(i);
+        let prefix = word.substring(0,i);
+        let suffix = word.substring(i);
 
-      let arrOfArrs = dict[prefix];
-      if(typeof arrOfArrs != 'undefined') {
-        arrOfArrs.forEach(arr => {
-          if(arr.includes(suffix)) {
-            let resItem = { prefix: prefix, suffices: arr };
-            ret.push(resItem);
-          }
-        });
-      }
+        let arrOfArrs = dict[prefix];
+        if(typeof arrOfArrs != 'undefined') {
+            arrOfArrs.forEach(arr => {
+                if(arr.includes(suffix)) {
+                    let resItem = { prefix: prefix, suffices: arr };
+                    ret.push(resItem);
+                }
+            });
+        }
     }
     if(ret.length == 0)
-      return [{ prefix: word, suffices: [""] }];
+        return [{ prefix: word, suffices: [""] }];
     else {
-      return ret;
+        return ret;
     }
-  }
+}
+
+app.use(cors());
 
 app.get("/wordfamilies/:word", (req, res, next) => {    
     let ret = {}
